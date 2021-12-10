45ff9de78f9415b6c33a2557c4e2e531d6cf977c
Alessandro Scola <consulenzeitalia@gmail.com>
2021-12-10 17:09:37 +0100

    config changed to main folder

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 // BASE URL OF THE SITE
-define('CONFIG_BASE_URL', 'http://localhost/web-kiosk-pdf-viewer');
+define('CONFIG_BASE_URL', 'http://localhost/web-kiosk-pdf-viewer-main');
 $HOME=CONFIG_BASE_URL;
 ?>
